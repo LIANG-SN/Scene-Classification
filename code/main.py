8c416a5aedf1843e988607687b10c5e33f6b8744
LIANG-SN <liangsn2001@outlook.com>
2021-04-11 12:53:53 +0800

    visual word computing

--- code/main.py
+++ code/main.py
@@ -1,26 +1,42 @@
 import numpy as np
-import torchvision
+import numpy.matlib as npm
+# import torchvision
 import util
 import matplotlib.pyplot as plt
 import visual_words
-import visual_recog
-import deep_recog
+# import visual_recog
+# import deep_recog
 import skimage.io
 
 if __name__ == '__main__':
-
+   
 	num_cores = util.get_num_CPU()
 
+	# 1.1
 	path_img = "../data/kitchen/sun_aasmevtpkslccptd.jpg"
-	image = skimage.io.imread(path_img)
-	image = image.astype('float')/255
-	filter_responses = visual_words.extract_filter_responses(image)
-	util.display_filter_responses(filter_responses)
-
-	#visual_words.compute_dictionary(num_workers=num_cores)
+	# path_img = "../data/kitchen/sun_agrkvwbkgrglcoga.jpg"
+	# path_img = "../data/kitchen/sun_aaebjpeispxohmfv.jpg"
 	
-	#dictionary = np.load('dictionary.npy')
-	#img = visual_words.get_visual_words(image,dictionary)
+	
+	image = skimage.io.imread(path_img)
+	# image = image.astype('float') / 255
+	# filter_responses = visual_words.extract_filter_responses(image)
+	# util.display_filter_responses(filter_responses)
+
+	# 1.2
+	# visual_words.compute_dictionary(num_workers=num_cores)
+	# data = np.load('dictionary.npy')
+	# print(data.shape)
+	
+	# 1.3 test wordmaps
+	plt.subplot(2, 1, 1)
+	plt.imshow(image)
+	dictionary = np.load('dictionary.npy')
+	wordmap = visual_words.get_visual_words(image,dictionary)
+	plt.subplot(2, 1, 2)
+	plt.imshow(wordmap, cmap="nipy_spectral")
+	plt.show()
+
 	#util.save_wordmap(wordmap, filename)
 	#visual_recog.build_recognition_system(num_workers=num_cores)
 
--- code/visual_words.py
+++ code/visual_words.py
@@ -12,23 +12,49 @@ import random
 
 def extract_filter_responses(image):
 	'''
-	Extracts the filter responses for the given image.
-
+	Extracts the filter responses for the given image.	
 	[input]
 	* image: numpy.ndarray of shape (H,W) or (H,W,3)
 	[output]
 	* filter_responses: numpy.ndarray of shape (H,W,3F)
 	'''
-	
-	# ----- TODO -----
-	
-	pass
-
-
-
-
-
-
+	# F = 20 ?
+	h = image.shape[0]
+	w = image.shape[1]
+	# check color channel
+	if image.ndim == 2:
+		image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+		print(image.shape)
+		# review this
+	if image.shape[2] == 4:
+		image = image[:, :, :3]
+	# check rgb value range
+	if image.max() > 1.0:
+		image = image.astype('float') / 255
+	# convert rgb to lab
+	image = skimage.color.rgb2lab(image)
+	# filter
+	scales = np.array([1, 2, 4, 8, 8 * np.sqrt(2)])
+	output = np.zeros((h, w, 3 * 20))
+	i = 0
+	for scale in scales:
+		output[:, :, i * 3]     = scipy.ndimage.gaussian_filter(image[:, :, 0], scale)
+		output[:, :, i * 3 + 1] = scipy.ndimage.gaussian_filter(image[:, :, 1], scale)
+		output[:, :, i * 3 + 2] = scipy.ndimage.gaussian_filter(image[:, :, 2], scale)
+		i += 1
+		output[:, :, i * 3]     = scipy.ndimage.gaussian_laplace(image[:, :, 0], scale)
+		output[:, :, i * 3 + 1] = scipy.ndimage.gaussian_laplace(image[:, :, 1], scale)
+		output[:, :, i * 3 + 2] = scipy.ndimage.gaussian_laplace(image[:, :, 2], scale)
+		i += 1
+		output[:, :, i * 3]     = scipy.ndimage.gaussian_filter(image[:, :, 0], scale, (0, 1))
+		output[:, :, i * 3 + 1] = scipy.ndimage.gaussian_filter(image[:, :, 1], scale, (0, 1))
+		output[:, :, i * 3 + 2] = scipy.ndimage.gaussian_filter(image[:, :, 2], scale, (0, 1))
+		i += 1
+		output[:, :, i * 3]     = scipy.ndimage.gaussian_filter(image[:, :, 0], scale, (1, 0))
+		output[:, :, i * 3 + 1] = scipy.ndimage.gaussian_filter(image[:, :, 1], scale, (1, 0))
+		output[:, :, i * 3 + 2] = scipy.ndimage.gaussian_filter(image[:, :, 2], scale, (1, 0))
+		i += 1
+	return output
 def get_visual_words(image,dictionary):
 	'''
 	Compute visual words mapping for the given image using the dictionary of visual words.
@@ -39,9 +65,17 @@ def get_visual_words(image,dictionary):
 	[output]
 	* wordmap: numpy.ndarray of shape (H,W)
 	'''
-	
-	# ----- TODO -----
-	pass
+
+	filtered = extract_filter_responses(image)
+	h, w, f = filtered.shape
+	filtered = filtered.reshape(h*w, f)
+	dist_matrix = scipy.spatial.distance.cdist(filtered, dictionary, 'euclidean')
+	assert dist_matrix.shape == (h*w, dictionary.shape[0])
+	wordmap = np.zeros(h*w)
+	wordmap[:] = dist_matrix[:].argmin(axis=-1)
+	wordmap = wordmap.reshape(h, w)
+	return wordmap
+
 
 
 def compute_dictionary_one_image(args):
@@ -61,9 +95,16 @@ def compute_dictionary_one_image(args):
 
 
 	i,alpha,image_path = args
-	# ----- TODO -----
-	pass
 
+	im = imageio.imread(image_path)
+	h = im.shape[0]
+	w = im.shape[1]
+	samplesX = random.sample(range(0, w), alpha)
+	samplesY = random.sample(range(0, h), alpha)
+	sampled_response = np.zeros((alpha, 60))
+	sampled_response[:] = extract_filter_responses(im)[samplesY[:], samplesX[:]]
+	fileName = '../temporary_data/temp'.strip() + str(i).strip()
+	np.save(fileName, sampled_response)
 
 def compute_dictionary(num_workers=2):
 	'''
@@ -76,8 +117,26 @@ def compute_dictionary(num_workers=2):
 	* dictionary: numpy.ndarray of shape (K,3F)
 	'''
 
-	train_data = np.load("../data/train_data.npz")
-	# ----- TODO -----
-	pass
+	train_data = np.load("../data/train_data.npz", allow_pickle=True)
 
+	T = train_data['image_names'].shape[0]
+	alpha = 50
+	K = 300
+	# compute filter responses
 
+	for i in range(T):
+		# improve this
+		path = ('../data/'.strip() + str((train_data['image_names'][i]))[2:-2])
+		compute_dictionary_one_image((i, alpha, path))
+		print('compute' + str(i))
+	
+	# load all filter responese
+	filter_responses = np.zeros((alpha * T, 60))
+	for i in range(T):
+		fileName = '../temporary_data/temp'.strip() + str(i).strip() + '.npy'.strip()
+		filter_responses[i * alpha : i * alpha + alpha] = np.load(fileName)
+		print('load' + str(i))
+
+	kmeans = sklearn.cluster.KMeans(n_clusters=K, n_jobs=num_workers).fit(filter_responses)
+	dictionary = kmeans.cluster_centers_
+	np.save('dictionary.npy', dictionary)
